b9b9071e54f04c701bbb1b1e62b22a3d8ad466af
20817057 <IAAbdreev@sberbank.ru>
2023-10-07 11:49:06 +0300

    Дописал тест проверки "Получить данные для вкладки коллеги" статистика

--- api/goals_and_estimate/goals/goals_statistic.py
+++ api/goals_and_estimate/goals/goals_statistic.py
@@ -1,7 +1,5 @@
-from uuid import UUID
 from requests import Response
 from api.custom_requests import Request
-# from dto.app_goals.sc-hema.goal_enum import Period
 from services import Services
 
 
@@ -15,18 +13,7 @@ class Statistics(Request):
         self.headers.update({"Content-Type": "application/json"})
         self.url = f'{self.url}/api/v{version}/goals/statistics/board'
 
-    # def get_statistics_direct_widget(self, year: int, period) -> Response:
-    #     """Получить данные для вкладки прямое подчинение"""
-    #     return self.request(method='GET', url=f'{self.url}/direct/widget', params={'year': year, 'period': period})
 
-    # def get_statistics_direct_table(self, period, year: int, page_number: int, page_size: int) -> Response:
-    #     """Получить данные для вкладки таблица прямое подчинение"""
-    #     return self.request(
-    #         method='GET',
-    #         url=f'{self.url}/direct/table',
-    #         params={'year': year, 'period': period, 'pageNumber': page_number, 'pageSize': page_size}
-    #     )
-    #
     def get_statistics_team_widget(self, year: int, period: str) -> Response:
         """Получить данные для вкладки коллеги - виджет"""
         return self.request(method='GET', url=f'{self.url}/team/widget', params={'year': year, 'period': period})
@@ -38,32 +25,3 @@ class Statistics(Request):
             url=f'{self.url}/team/table',
             params={'year': year, 'period': period, 'pageNumber': page_number, 'pageSize': page_size}
         )
-    #
-    # def get_statistics_structure_widget(self, period, year: int, department_id: UUID) -> Response:
-    #     """Получить данные для вкладки структура - виджет"""
-    #     return self.request(
-    #         method='GET',
-    #         url=f'{self.url}/structure/widget',
-    #         params={'year': year, 'period': period, 'departmentId': department_id}
-    #     )
-
-    # def get_statistics_structure_tabel(
-    #         self,
-    #         period: ,
-    #         year: int,
-    #         department_id: UUID,
-    #         page_number: int,
-    #         page_size: int
-    # ) -> Response:
-    #     """Получить данные для вкладки структура - таблица"""
-    #     return self.request(
-    #         method='GET',
-    #         url=f'{self.url}/structure/table',
-    #         params={
-    #            'period': period,
-    #            'year': year,
-    #            'departmentId': department_id,
-    #            'pageNumber': page_number,
-    #            'pageSize': page_size
-    #         }
-    #     )
--- tests/goals_and_estimate/goals/api/test_goal_statistic.py
+++ tests/goals_and_estimate/goals/api/test_goal_statistic.py
@@ -1,20 +1,10 @@
 import datetime
-
 from pytest import mark
-
 from allure import story, title, link
 from api.goals_and_estimate.goals.goals_statistic import Statistics
-# from dto.app_goals.schema.goal_dto import OrgTeamShortResponse
-# from dto.app_goals.schema.goal_enum import Period
-# from dto.app_goals.schema.statistics_dto import (
-#     StatisticsBoardDirectTableResponseV2,
-#     StatisticsBoardStructureTableResponseV2,
-#     StatisticsBoardTeamTableResponseV2,
-#     StatisticsBoardWidgetResponseV2
-# )
 from tests.constants import ERROR_BODY_VALID_MSG, ERROR_STATUS_MSG
 from tests.goals_and_estimate.allure_constants import GoalsAPI
-from users.goals import DIRECTOR_GOALS
+from users.goals import EMPLOYEE_GOALS
 from utilities import model
 from utilities.tools import get_link
 
@@ -22,7 +12,7 @@ from utilities.tools import get_link
 @story('Проверки на получение данных для вкладки статистика')
 @mark.dpm
 @mark.usefixtures('set_up', 'auth_api')
-@mark.parametrize('auth_api', [DIRECTOR_GOALS], indirect=True)
+@mark.parametrize('auth_api', [EMPLOYEE_GOALS], indirect=True)
 class TestGoalStatistics(GoalsAPI):
     current_year: int
 
@@ -31,92 +21,25 @@ class TestGoalStatistics(GoalsAPI):
         TestGoalStatistics.current_year = datetime.datetime.now().year
 
     # @mark.prom
-    # @link(*get_link(test=41155))
-    # @title('Получить данные для вкладки прямое подчинение')
-    # @mark.parametrize('period')
-    # def test_get_statistics_direct_widget(self):
-    #     assert (
-    #        response := Statistics(version=2).get_statistics_direct_widget(year=self.current_year, period='Q1')
-    #     ).status_code == 200, ERROR_STATUS_MSG.format(code=response.status_code)
-        # assert model.is_valid(model=StatisticsBoardWidgetResponseV2, response=response.json()), ERROR_BODY_VALID_MSG
-
-    # @mark.prom
-    # @link(*get_link(test=41156))
-    # @title('Получить данные для вкладки таблица прямое подчинение')
-    # # @mark.parametrize('period', Period)
-    # @mark.parametrize('page_size', [10, 20, 50])
-    # def test_get_statistics_direct_widget(self, period, page_size: int):
-    #     assert (
-    #        response := Statistics(version=2).get_statistics_direct_table(
-    #            year=self.current_year,
-    #            period=period,
-    #            page_number=1,
-    #            page_size=page_size
-    #        )
-    #     ).status_code == 200, ERROR_STATUS_MSG.format(code=response.status_code)
-    #     # assert model.is_valid(
-    #     #     model=StatisticsBoardDirectTableResponseV2, response=response.json()
-    #     # ), ERROR_BODY_VALID_MSG
-    #
-    # @mark.prom
     # @link(*get_link(test=41153))
     @title('Получить данные для вкладки коллеги - виджет')
     # @mark.parametrize('period')
     def test_get_statistics_team_widget(self, auth_api: str):
-
         response = Statistics(version=2).get_statistics_team_widget(year=self.current_year, period='Q1')
         assert response.status_code == 200, ERROR_STATUS_MSG.format(code=response.status_code)
-        # assert model.is_valid(model=StatisticsBoardWidgetResponseV2, response=response.json()), ERROR_BODY_VALID_MSG
 
-    @mark.prom
-    @link(*get_link(test=41154))
+    # @mark.prom
+    # @link(*get_link(test=41154))
     @title('Получить данные для вкладки коллеги - таблица')
     # @mark.parametrize('period')
     @mark.parametrize('page_size', [10, 20, 50])
     def test_get_statistics_team_table(self, page_size: int):
         assert (
-           response := Statistics(version=2).get_statistics_team_table(
-               period='Q1',
-               year=self.current_year,
-               page_number=1,
-               page_size=page_size
-           )
-        ).status_code == 200, ERROR_STATUS_MSG.format(code=response.status_code)
+                   response := Statistics(version=2).get_statistics_team_table(
+                       period='Q1',
+                       year=self.current_year,
+                       page_number=1,
+                       page_size=page_size
+                   )
+               ).status_code == 200, ERROR_STATUS_MSG.format(code=response.status_code)
         print(response.json())
-        # assert model.is_valid(model=StatisticsBoardTeamTableResponseV2, response=response.json()), ERROR_BODY_VALID_MSG
-
-    # @link(*get_link(test=43802))
-    # @title('Получить данные для вкладки структура - виджет')
-    # @mark.parametrize('period')
-    # def test_get_statistics_structure_widget(self, period, orgstructure_team: list[OrgTeamShortResponse]):
-    #     assert (
-    #        response := Statistics(version=2).get_statistics_structure_widget(
-    #            year=self.current_year,
-    #            period=period,
-    #            department_id=orgstructure_team[0].id
-    #        )
-    #     ).status_code == 200, ERROR_STATUS_MSG.format(code=response.status_code)
-    #     assert model.is_valid(model=StatisticsBoardWidgetResponseV2, response=response.json()), ERROR_BODY_VALID_MSG
-    #
-    # @link(*get_link(test=43803))
-    # @title('Получить данные для вкладки структура - таблица')
-    # @mark.parametrize('period', Period)
-    # @mark.parametrize('page_size', [10, 20, 50])
-    # def test_get_statistics_structure_tabel(
-    #         self,
-    #         period: Period,
-    #         page_size: int,
-    #         orgstructure_team: list[OrgTeamShortResponse]
-    # ):
-    #     assert (
-    #        response := Statistics(version=2).get_statistics_structure_tabel(
-    #            period=period,
-    #            year=self.current_year,
-    #            department_id=orgstructure_team[0].id,
-    #            page_number=1,
-    #            page_size=page_size
-    #        )
-    #     ).status_code == 200, ERROR_STATUS_MSG.format(code=response.status_code)
-    #     assert model.is_valid(
-    #         model=StatisticsBoardStructureTableResponseV2, response=response.json()
-    #     ), ERROR_BODY_VALID_MSG
