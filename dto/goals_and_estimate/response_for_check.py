f2d9dcf8ba0f65ed1117d909a50041fc2dfdd262
20817057 <IAAbdreev@sberbank.ru>
2023-10-23 16:29:47 +0300

    Ответы при создании и запросе целей

---
+++ dto/goals_and_estimate/response_for_check.py
@@ -0,0 +1,187 @@
+# создание
+{
+    "id": 10839,
+    "title": "новая цель",
+    "description": "",
+    "reporter": {
+        "uuid": "7227d3cd-bc43-4e5e-81b9-506a4d2cfcad",
+        "firstName": "Name687",
+        "lastName": "Surname486",
+        "middleName": "Middlename801",
+        "fullName": "Surname486 Name687 Middlename801",
+        "positionShortName": "Директор по ИТ",
+        "positionFullName": "Директор по ИТ",
+        "photoUrl": "",
+        "gender": "M",
+        "status": "AUTHOR",
+        "comment": ""
+    },
+    "responsible": {
+        "uuid": "7227d3cd-bc43-4e5e-81b9-506a4d2cfcad",
+        "firstName": "Name687",
+        "lastName": "Surname486",
+        "middleName": "Middlename801",
+        "fullName": "Surname486 Name687 Middlename801",
+        "positionShortName": "Директор по ИТ",
+        "positionFullName": "Директор по ИТ",
+        "photoUrl": "",
+        "gender": "M",
+        "status": "AUTHOR",
+        "comment": ""
+    },
+    "assignees": [
+        {
+            "uuid": "7227d3cd-bc43-4e5e-81b9-506a4d2cfcad",
+            "firstName": "Name687",
+            "lastName": "Surname486",
+            "middleName": "Middlename801",
+            "fullName": "Surname486 Name687 Middlename801",
+            "positionShortName": "Директор по ИТ",
+            "positionFullName": "Директор по ИТ",
+            "photoUrl": "",
+            "gender": "M",
+            "status": "AUTHOR",
+            "comment": ""
+        },
+        {
+            "uuid": "7227d3cd-bc43-4e5e-81b9-506a4d2cfcad",
+            "firstName": "Name687",
+            "lastName": "Surname486",
+            "middleName": "Middlename801",
+            "fullName": "Surname486 Name687 Middlename801",
+            "positionShortName": "Директор по ИТ",
+            "positionFullName": "Директор по ИТ",
+            "photoUrl": "",
+            "gender": "M",
+            "status": "RESPONSIBLE",
+            "comment": ""
+        }
+    ],
+    "endDate": "2023-12-31",
+    "startDate": "2023-01-01",
+    "periodStart": "Q1",
+    "periodEnd": "Q4",
+    "yearGoal": false,
+    "visibility": "VISIBLE",
+    "childrenGoals": [],
+    "parentGoal": null,
+    "keyResults": {
+        "countAllKR": 0,
+        "countKRByPeriod": 0,
+        "keyResultInProgress": [],
+        "keyResultCompleted": []
+    },
+    "progress": 0,
+    "fireState": "NOT_FIRE",
+    "isRiskState": false,
+    "riskComment": "",
+    "isFreeze": false,
+    "type": "STANDARD",
+    "files": [],
+    "weight": {
+        "goalId": 10839,
+        "uuid": "7227d3cd-bc43-4e5e-81b9-506a4d2cfcad",
+        "Q1": 0.0,
+        "Q2": 0.0,
+        "Q3": 0.0,
+        "Q4": 0.0,
+        "Y": 0.0
+    }
+}
+Получение
+{
+    "goal": {
+        "id": 10839,
+        "title": "новая цель",
+        "description": "",
+        "assignees": {
+            "author": {
+                "uuid": "7227d3cd-bc43-4e5e-81b9-506a4d2cfcad",
+                "firstName": "Name687",
+                "lastName": "Surname486",
+                "middleName": "Middlename801",
+                "fullName": "Surname486 Name687 Middlename801",
+                "positionShortName": "Директор по ИТ",
+                "positionFullName": "Директор по ИТ",
+                "photoUrl": "",
+                "gender": "M",
+                "status": "AUTHOR",
+                "comment": ""
+            },
+            "responsible": {
+                "uuid": "7227d3cd-bc43-4e5e-81b9-506a4d2cfcad",
+                "firstName": "Name687",
+                "lastName": "Surname486",
+                "middleName": "Middlename801",
+                "fullName": "Surname486 Name687 Middlename801",
+                "positionShortName": "Директор по ИТ",
+                "positionFullName": "Директор по ИТ",
+                "photoUrl": "",
+                "gender": "M",
+                "status": "RESPONSIBLE",
+                "comment": ""
+            },
+            "accepted": [],
+            "requested": [],
+            "rejected": []
+        },
+        "startDate": "2023-01-01",
+        "endDate": "2023-12-31",
+        "periodStart": "Q1",
+        "periodEnd": "Q4",
+        "yearGoal": false,
+        "visibility": "VISIBLE",
+        "keyResults": {
+            "countAllKR": 0,
+            "countKRByPeriod": 0,
+            "keyResults": []
+        },
+        "progress": {
+            "progress": 0,
+            "q1": {
+                "progressWithDisableWeight": 0.0,
+                "progressWithWeight": 0.0
+            },
+            "q2": {
+                "progressWithDisableWeight": 0.0,
+                "progressWithWeight": 0.0
+            },
+            "q3": {
+                "progressWithDisableWeight": 0.0,
+                "progressWithWeight": 0.0
+            },
+            "q4": {
+                "progressWithDisableWeight": 0.0,
+                "progressWithWeight": 0.0
+            },
+            "qyear": {
+                "progressWithDisableWeight": 0.0,
+                "progressWithWeight": 0.0
+            }
+        },
+        "fire": {
+            "state": "NOT_FIRE",
+            "countFireCalendarKR": 0,
+            "countFireKR": 0
+        },
+        "isRiskState": false,
+        "riskComment": "",
+        "isFreeze": false,
+        "type": "STANDARD",
+        "approveStatus": "NOT_AGREED",
+        "files": []
+    },
+    "weight": {
+        "Q1": 0.0,
+        "Q2": 0.0,
+        "Q3": 0.0,
+        "Q4": 0.0,
+        "Y": 0.0
+    },
+    "permissionGoal": {
+        "canEditGoal": true,
+        "canEditWeight": true,
+        "canAcceptRequest": false,
+        "canDeleteGoal": true
+    }
+}
